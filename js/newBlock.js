1bc3478039cd9afab2d130d6c79ea5418c89b578
|HACKERMAN| <maysonlive1@gmail.com>
2021-03-18 20:23:22 +0300

    Save data, update location

--- js/newBlock.js
+++ js/newBlock.js
@@ -1,12 +1,16 @@
 var request = new XMLHttpRequest();
-var localStorage = [];
+let ItemArray = [];
+let apiKey = "ebfa87e4d112fd5614094678d7b9224d";
 
-function onload(){
+function hideLoadingLayer(){
     //let new_city = document.getElementById("new_city");
+    document.getElementById("loading_layer").style.visibility="hidden";
+    alert("hi");
 }
 
 function deleteBlock(but){
   let city = but.id;
+  localStorage.removeItem(city);
   let elem = document.getElementById(city);
   elem.parentNode.removeChild(elem);
 }
@@ -21,7 +25,7 @@ request.open ("GET", url, true);
 request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(request.responseText);
-      console.log(data);//imgClassBig
+      //console.log(data);//imgClassBig
       var imgSmall = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png"></img>`;
       var weather__city = data.name;
       var temperature = data.main.temp + "°C";
@@ -30,7 +34,9 @@ request.onload = function() {
       var feels_like = data.main.feels_like + "°C";
       var pressure = data.main.pressure;
       var country = data.sys.country; //<img class="imgClassSmall" src="https://img.icons8.com/material/96/000000/cloud--v1.png"></img>
-      AddNewForm(temperature,imgSmall,humidity,wind_speed,feels_like,pressure,country);
+      AddNewForm(temperature,imgSmall,humidity,wind_speed,feels_like,pressure,country,weather__city);
+      localStorage.setItem(weather__city, weather__city);
+      //ItemArray.push(weather__city)
     } else {
       console.log("Error with api!");
       alert("Города нет. Прости(");
@@ -39,16 +45,15 @@ request.onload = function() {
 request.send();
     }
 
-    function AddNewForm(temperature,imgSmall,humidity,wind_speed,feels_like,pressure,country){
-      //localStorage.push(new_city.value);
+    function AddNewForm(temperature,imgSmall,humidity,wind_speed,feels_like,pressure,country,weather__city){
       add_to_me.insertAdjacentHTML('beforeend',` 
-      <div id="`+new_city.value+`">
+      <div id="`+weather__city+`">
       <ul class="weatherData">
       <div class="hat">
-      <h3 class="CityStyle">`+new_city.value+`</h3>
+      <h3 class="CityStyle">`+weather__city+`</h3>
       <p class="tempPsmall">`+temperature+`</p>
       `+imgSmall+`
-      <button onclick="deleteBlock(this)" id="`+new_city.value+`" class="supreme">x</button>
+      <button onclick="deleteBlock(this)" id="`+weather__city+`" class="supreme">x</button>
       </div>
       <li><div class="weatherBlock"><p class="p1">Влажность</p><p class="p2">`+humidity+`</p></div></li>
       <li><div class="weatherBlock"><p class="p1">Скорость ветра</p><p class="p2">`+wind_speed+`</p></div></li>
@@ -60,8 +65,6 @@ request.send();
   
   `);
     }
-
-    let apiKey = "ebfa87e4d112fd5614094678d7b9224d";
     
     if (!navigator.geolocation) 
     {
@@ -93,7 +96,7 @@ request.send();
             request.onload = function() {
                 if (request.status >= 200 && request.status < 400) {
                   var data = JSON.parse(request.responseText);
-                  console.log(data);//imgClassBig
+                  //console.log(data);//imgClassBig
                   document.querySelector('.imgClassBig').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png"></img>`;
                   document.querySelector('.cur_weather__city').textContent = data.name;
                   document.querySelector('.tempP').innerHTML = data.main.temp + "°C";
@@ -109,3 +112,73 @@ request.send();
               
         request.send();
     }
+
+
+  function UpdateLocation(){
+
+    navigator.geolocation.getCurrentPosition(
+      function (location) {
+          let lat  = location.coords.latitude;
+          let lon = location.coords.longitude;
+          //api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+          var url = new URL("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apiKey+"&units=metric");
+
+          GetMainData(url);
+      },
+      function () {
+          alert("Доступ к геопозиции запрещен")
+          let city = "moscow";
+          var url = new URL("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey+"&units=metric");
+
+          GetMainData(url);
+      }
+  )
+
+  }
+
+  var count = 0;
+  function autoLoad(){
+    if(localStorage.length != 0){
+      if(localStorage.length > count){
+        console.log(localStorage.key(count));
+        let curCity = localStorage.key(count)
+        addCodeNew(curCity);
+        count++;
+        autoLoad();
+      }
+      else{
+        console.log("X");
+      }
+    }
+  }
+
+
+  function addCodeNew(city) {  
+    var subRequest = new XMLHttpRequest();
+    console.log("test");
+    let url = new URL("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey+"&units=metric");
+  
+    subRequest.open ("GET", url, true);
+  
+    subRequest.onload = function() {
+      if (subRequest.status >= 200 && subRequest.status < 400) {
+        var data = JSON.parse(subRequest.responseText);
+        //console.log(data);//imgClassBig
+        var imgSmall = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png"></img>`;
+        var weather__city = data.name;
+        var temperature = data.main.temp + "°C";
+        var humidity = data.main.humidity;
+        var wind_speed = data.wind.speed + "м/с";
+        var feels_like = data.main.feels_like + "°C";
+        var pressure = data.main.pressure;
+        var country = data.sys.country; //<img class="imgClassSmall" src="https://img.icons8.com/material/96/000000/cloud--v1.png"></img>
+        AddNewForm(temperature,imgSmall,humidity,wind_speed,feels_like,pressure,country,weather__city);
+        //localStorage.setItem(weather__city, weather__city);
+        //ItemArray.push(weather__city)
+      } else {
+        console.log("Error with api!");
+        alert("Города нет. Прости(");
+      }
+    };
+    subRequest.send();
+  }
